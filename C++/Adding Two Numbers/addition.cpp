344aa4746fb493f0e68a8f08a32f251af0197cdd
Meheru Islam Nishat <nishat-25024018@aaub.edu.bd>
2025-11-24 15:24:02 +0600

    Add files via upload

---
+++ C++/Adding Two Numbers/addition.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout << "Enter first integers: ";
+    cin >> a;
+    cout << "Enter second integers: ";
+    cin>> b;
+    int sum;
+    sum = a + b;
+    cout << "The sum of " << a << " and " << b << " is " << sum << endl;
+    return 0;
+}
